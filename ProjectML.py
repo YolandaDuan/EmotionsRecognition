41714f04b7aa1b58ec664b6ce6be8d3ebb3d7761
yolanda <dzm13546157880@gmail.com>
2019-05-09 02:16:56 +0200

    add plot and minor changes

--- ProjectML.py
+++ ProjectML.py
@@ -1,13 +1,24 @@
 import os
 import numpy as np
+import pandas as pd
+
 import matplotlib as mpl
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 from sklearn.decomposition import PCA
 from sklearn import manifold, neighbors, metrics
 from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.svm import LinearSVC
+
 from scipy.spatial import distance
 
+'''
+Global Variables 
+'''
+SIMPLE_EMBEDDING=False
+MANUAL_SPLIT    =True
+DATA_2D         =True
 
 labels ={
     "ANGER": 0,
@@ -22,6 +33,9 @@ class classData:
     def __init__(self,dir=None):
         if dir:
             data,label,name = readLm3(dir)
+            print(np.shape(data))
+            print(np.shape(label))
+            print(np.shape(name))
             self.features = np.array(data)
 
             self.label = [labels[i] for i in label]
@@ -35,7 +49,7 @@ class classData:
         self.n_files = 0
 
 
-def knnClassifier(data,labels, nNeighbors=20):
+def knnClassifier(data,labels, nNeighbors):
     knn = neighbors.KNeighborsClassifier(nNeighbors, weights='distance')
     knn.fit(data,labels)
     return knn
@@ -49,20 +63,57 @@ def testClass(classifier, classData, testData, testLabels):
 
     prediction = classifier.predict(testData)
     accuracy = metrics.accuracy_score(testLabels, prediction)
-    print("\n Accuracy: %f",accuracy)
-    print("\n Results from classifier:  \n %s \n"
+    print("Accuracy: ",accuracy)
+    print("Results from classifier:  \n %s \n"
           % ( metrics.classification_report(testLabels, prediction)))
-    print("\n Confussion matrix:\n %s" % metrics.confusion_matrix(testLabels, prediction))
-    kfold = 10
+    print("Confussion matrix:\n %s" % metrics.confusion_matrix(testLabels, prediction))
+    kfold = 5
     scores = cross_val_score(classifier, classData.features, classData.label, cv=kfold)
-    print("\n Cross validation score: \n")
+    print("Cross validation score: ")
     print(scores)
-    print("\n Mean cv score: %f", np.mean(scores))
+    print("Mean cv score: ", np.mean(scores))
 
-def holdOut(dataClass, percentSplit):
+""" def holdOut(dataClass, percentSplit):
     trainData, testData, trainLabels, expectedLabels = train_test_split(dataClass.features, dataClass.label,
                                                                        test_size=(1.0 - percentSplit), random_state=0)
+    return trainData,testData,trainLabels,expectedLabels """
+
+def holdOut(dataClass, nSamples, percentSplit=0.6):
+    '''
+    This function splits the data into training and test sets
+    '''
+    if(MANUAL_SPLIT):
+        n_trainSamples = int(nSamples * percentSplit)
+        trainData = dataClass.features[:n_trainSamples]
+        trainLabels = dataClass.label[:n_trainSamples]
+        
+        testData = dataClass.features[n_trainSamples:]
+        expectedLabels = dataClass.label[n_trainSamples:]
+    else:
+        trainData, testData, trainLabels, expectedLabels = train_test_split(dataClass.features, dataClass.label,
+                                                                            test_size=(1.0-percentSplit), random_state=0)
+
     return trainData,testData,trainLabels,expectedLabels
+
+def plotData(X, labelsData):
+    '''
+    This function plots either 2D data or 3D data
+    '''
+    if(DATA_2D):
+        # Convert labels from string into integer, then plot it in different colors
+        labelsData = pd.Categorical(pd.factorize(labelsData)[0])
+        plt.scatter(X[:,0], X[:,1])
+        plt.show()
+    else:
+        fig = plt.figure()
+        ax = Axes3D(fig)
+        ax.scatter(X[:,0], X[:,1], X[:,2])
+        
+        ax.set_xlabel('X Label')
+        ax.set_ylabel('Y Label')
+        ax.set_zlabel('Z Label')
+        
+        plt.show()
 
 # Read Files
 def readLm3(directory):
@@ -76,7 +127,7 @@ def readLm3(directory):
     for file in os.listdir(path):
         if file.endswith(".lm3"):
             files.append(str(file))
-    print(files)
+    # print(files)
     for file in files:
         lm3Data = parseLm3File(path+"/"+file)
         if lm3Data:
@@ -140,13 +191,30 @@ path = "dataBosphoruslm3"
 
 dataClass = classData(path)
 dataClass.features = dataClass.features.reshape(dataClass.features.shape[0], -1)
+n_samples = len(dataClass.features)
+print(dataClass)
+print(dataClass.features)
+print(dataClass.features.shape)
 
-trainData, testData, trainLabels, expectedLabels = holdOut(dataClass, 0.6)
-knn = knnClassifier(trainData,trainLabels,10)
+# Data Reduction 
+if(SIMPLE_EMBEDDING):
+    #  Dimensionality Reduction PCA 
+    pca = PCA(n_components = 2)
+    X_trans = pca.fit_transform(dataClass.features)
+else:
+    # Manifold embedding with tSNE/Users/Lingyan/Desktop/IIS2019/Assignment 1/emotions_recognition.py
+    tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+    X_trans = tsne.fit_transform(dataClass.features)
+
+# Ploting Data
+plotData(X_trans, dataClass.label)
+
+trainData, testData, trainLabels, expectedLabels = holdOut(dataClass, n_samples)
+knn = knnClassifier(trainData,trainLabels,6)
 svm = svmClassifier(trainData,trainLabels)
-print("\n Report for K-NN classifier: \n")
+print("\nReport for K-NN classifier\n")
 testClass(knn, dataClass,testData,expectedLabels)
-print("\n Report for SVM classifier: \n")
+print("\nReport for SVM classifier\n")
 testClass(svm, dataClass,testData,expectedLabels)
 
 
